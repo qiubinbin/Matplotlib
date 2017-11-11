94580a61546fe85bbee4a44ce1f63c11b574c9f0
qiubin <qiubin780061109@163.com>
2017-11-11 15:02:50 +0800

    v1.1

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,10 +1,19 @@
 import matplotlib.pyplot as plt
+from random_walk import RandomWalk
 
-x = range(6)
-y = [0, 1, 4, 9, 16, 25]
-plt.scatter(x, y, s=100, color='r')
-plt.title('Square Number', fontsize=14)
-plt.xlabel('Value', fontsize=14,color='r')
-plt.ylabel('Square of Value', fontsize=14)
-plt.tick_params(axis='both', which='minor', labelsize=10)
-plt.show()
+while True:
+	rw = RandomWalk(5000)
+	rw.fill_walk()  # 设置绘图窗口尺寸
+	plt.figure(figsize=(10, 10))
+	point_number = list(range(rw.num_points))
+	plt.plot(rw.x_values, rw.y_values, c='red', linewidth=0.1)  # 设置点的轮廓，半径
+	# 突出起始点
+	plt.scatter(rw.x_values[0], rw.y_values[0], c='green', s=40, edgecolors='none')
+	plt.scatter(rw.x_values[- 1], rw.y_values[- 1], c='green', s=40, edgecolors='none')
+	# 隐藏坐标轴
+	plt.axes().get_xaxis().set_visible(False)
+	plt.axes().get_yaxis().set_visible(False)
+	plt.show()
+	keep_run = input("Y/N?")
+	if keep_run == 'N':
+		break
